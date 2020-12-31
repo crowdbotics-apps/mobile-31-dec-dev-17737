00c3379d343d7e4f0cd0c61a3960c249ca6db9cf
Crowdbotics <team@crowdbotics.com>
2020-12-31 07:15:54 +0000

    add component 31 dec

---
+++ src/features/31Dec416650/screens/blank.js
@@ -0,0 +1,41 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+
+export default class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
+
+  render = () => (
+    <View>
+      <Image
+        source={{
+          uri:
+            "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/1_UwzJvq2.jpg"
+        }}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({ View_1: {}, Image_2: {} })
